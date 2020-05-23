ec825c1d920540b6917df393b08fd43f9672ad08
Artem Nikolaev <artem@nikolaev.by>
2020-05-23 22:46:19 +0300

    change logger configuration a bit

--- src/server/logger.js
+++ src/server/logger.js
@@ -1,10 +1,15 @@
-const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
 
-const logger = winston.createLogger({
+const logger = createLogger({
   level: 'silly',
-  format: winston.format.json(),
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    format.printf((info) => JSON.stringify(info, null, 2)),
+  ),
   transports: [
-    new winston.transports.Console({}),
+    new transports.Console({}),
   ],
 });
 
