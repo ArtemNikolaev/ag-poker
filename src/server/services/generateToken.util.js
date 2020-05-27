911fecb616653e69508d955b0d306630b3b617ee
Artem Nikolaev <artem@nikolaev.by>
2020-05-27 12:30:00 +0300

    promisify init functionality, add logs to socket.js and remove some redundant code

--- src/server/services/generateToken.util.js
+++ src/server/services/generateToken.util.js
@@ -1,37 +1,22 @@
 const crypto = require('crypto');
-const jwt = require('jsonwebtoken');
+
 const logger = require('./logger');
+const jwt = require('./jwt.interface');
 
-const place = 'generateToken';
+async function generateToken(id) {
+  logger.silly(`generateToken for ${id}`);
 
-module.exports = function generateToken (id, cb) {
-  logger.silly({
-    place,
-    msg: 'runned',
-    payload: { id },
-  })
-
-  logger.silly({
-    place,
-    msg: 'generating payload',
-  })
   const payload = {
     id,
     timestamp: Date.now(),
   };
 
-  logger.silly({
-    place,
-    msg: 'generating private key',
-  })
   const privateKey = crypto
     .createHash('md5')
     .update(String(Math.random()))
     .digest('hex');
 
-  logger.silly({
-    place,
-    msg: 'sign `payload` with `privateKey` and call cb',
-  })
-  jwt.sign(payload, privateKey, cb);
+  return jwt.sign(payload, privateKey);
 }
+
+module.exports = generateToken;
--- src/server/socket.js
+++ src/server/socket.js
@@ -1,20 +1,29 @@
 const socketio = require('socket.io');
-const init = require('./events/init.events');
+
+const logger = require('./services/logger');
+const generateToken = require('./services/generateToken.util');
 
 module.exports = function createSocket(httpServer) {
   const io = socketio(httpServer);
 
   io.on('connection', (socket) => {
-    console.log('a user connected: ', socket.id);
-    init(io, socket);
+    logger.debug(`new connection: ${socket.id}`);
 
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-    });
+    socket.on('init:token:request', async (token) => {
+      if (!token) {
+        token = await generateToken(socket.id);
 
-    socket.on('test', console.log);
+        logger.debug(`send to ${socket.id} generated token: ${token}`);
+        io.to(socket.id).emit('init:token:response', token);
+      }
+
+    //  register to appf
+    //     io.to(socket.id).emit('init:player:data', playerData);
+    //  send players data
+    })
+
+    socket.on('disconnect', (reason) => {
+      logger.debug(`${socket.id} disconnected because of: ${reason}`);
+    });
   });
 }
