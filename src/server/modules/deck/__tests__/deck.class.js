171129f8ffd1b6e85cc5678be14fe0238783b17f
Artem Nikolaev <artem@nikolaev.by>
2020-06-13 22:16:13 +0300

    roundLifeCycle - tests for _get and getDeckInfo

--- src/server/modules/deck/__tests__/deck.class.js
+++ src/server/modules/deck/__tests__/deck.class.js
@@ -1,6 +1,69 @@
-const deck = require('../deck.class');
+const Deck = require('../deck.class');
 
 describe('Deck class', () => {
+  let deck;
 
+  beforeEach(() => {
+    deck = new Deck();
+
+  });
+
+  describe('_get()', () => {
+    const splice = 'splice';
+
+    beforeEach(() => {
+      deck.deck = [1, 2];
+    });
+
+    describe('by default', () => {
+      it('should call splice with -1', () => {
+        deck.deck = {
+          splice: jest.fn(() => splice),
+        };
+        deck._get();
+
+        expect(deck.deck.splice).toHaveBeenCalledWith(-1);
+      });
+
+      it('played should be correct', () => {
+        deck._get();
+
+        expect(deck.played).toEqual([2]);
+      });
+
+      it('played should be correct', () => {
+        expect(deck._get()).toEqual([2]);
+      });
+    });
+
+    describe('with n', () => {
+      const n = 2;
+
+      it('with n argument', () => {
+        deck.deck = {
+          splice: jest.fn(() => splice),
+        };
+        deck._get(n);
+
+        expect(deck.deck.splice).toHaveBeenCalledWith(-n);
+      });
+
+      it('not by default', () => {
+        deck._get(n);
+
+        expect(deck.played).toEqual([1, 2]);
+      });
+
+      it('played should be correct', () => {
+        expect(deck._get(n)).toEqual([1, 2]);
+      });
+    });
+  });
+
+  it('getDeckInfo() should return deck', () => {
+    const deckInfo = require('../deck.json');
+
+    expect(deck.getDeckInfo()).toEqual(deckInfo);
+  });
 });
 
--- src/server/modules/deck/deck.class.js
+++ src/server/modules/deck/deck.class.js
@@ -7,37 +7,37 @@ class Deck extends EventEmitter {
 
     constructor() {
         super();
-        this.newDeck();
+        // this._newRound();
 
-        this.on('new:round', this._newRound());
-        this.on('shuffle', this._shuffle());
-        this.on('change:deck', this._changeDeck());
+        // this.on('new:round', this._newRound());
+        // this.on('shuffle', this._shuffle());
+        // this.on('change:deck', this._changeDeck());
     }
 
-    _shuffle = () => {
-        for (let i = this.cards.length - 1; i > 0; i++) {
-            const buffer = this.cards[i];
+    /*_shuffle = () => {
+        for (let i = this.deck.length - 1; i > 0; i++) {
+            const buffer = this.deck[i];
             const index = Math.trunc(Math.random() * (i - 1));
 
-            this.cards[i] = this.cards[index];
-            this.cards[index] = buffer;
+            this.deck[i] = this.deck[index];
+            this.deck[index] = buffer;
         }
-    }
+    }*/
 
-    _newRound = () => {
+    /*_newRound = () => {
         if (this.played.length) {
-            this.cards.push(...this.played);
+            this.deck.push(...this.played);
         }
 
         this._shuffle();
-    }
+    }*/
 
-    _changeDeck = () => {
-        this.deck = deckInfo.cards;
+    /*_changeDeck = () => {
+        this.deck = deckInfo.deck;
         this.played.splice(0);
 
         this.newGame();
-    }
+    }*/
 
 
     _get = (amount = 1) => {
@@ -47,10 +47,10 @@ class Deck extends EventEmitter {
         return cards;
     }
 
-    getHand = () => this._get(2)
-    getFlop = () => this._get(3)
-    getTurn = () => this._get()
-    getRiver = () => this._get()
+    // getHand = () => this._get(2)
+    // getFlop = () => this._get(3)
+    // getTurn = () => this._get()
+    // getRiver = () => this._get()
 
     getDeckInfo = () => deckInfo
 }
