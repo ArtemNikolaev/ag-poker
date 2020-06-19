1d63a43cb7ba593e16ad95bc3517984c1cc71474
Artem Nikolaev <artem@nikolaev.by>
2020-06-20 00:39:05 +0300

    roundLifeCycle - tests for _changeDeck

--- src/server/modules/game.js
+++ src/server/modules/game.js
@@ -2,6 +2,7 @@ const EventEmitter = require('events');
 
 const logger = require('../services/logger');
 const store = require('./players');
+const roundLifeCycle = require('./roundLifeCycle');
 
 /*
 * Statuses:
@@ -19,76 +20,27 @@ const store = require('./players');
 * determine the winner
 * */
 
+/*
+* Events:
+* next:move
+* next:round
+* */
 
 class Game extends EventEmitter {
-  isGameOn = false;
-  players = {};
-  sb = '';
-  bb = '';
-  bank = 0;
-  status = 'pending';
-
-  amount = 5000;
-
   constructor() {
     super();
+    this.nextRound();
 
-    store.on('new:player', () => {
-      if (this.isGameOn) {
-        logger.silly('Game is already started');
-        return;
-      }
-      if (store.players.size < 2) {
-        logger.silly('Players less than needed amount');
-        return;
-      }
-
-      logger.silly('begin the game');
-
-      this.isGameOn = true;
-      this._begin();
-    });
-
-    store.on('player:removed', (id) => {
-      if (this.players[id]) {
-        delete this.players[id];
-        logger.silly(`player ${id} removed`);
-
-        this._validate();
-        this._begin();
-      }
-    });
+    this.on('next:move', this.nextMove);
   }
 
-  setAmount(num) {
-    logger.silly(`minimal amound setted to ${num}`);
-    this.amount = num;
+  nextMove = () => {
+    const result = this.round.next();
+    if (result.done) this.nextRound();
   }
 
-  _validate() {
-    this.emit('players', this.players);
-
-    const length = Object.keys(this.players).length;
-
-    if (length < 2) {
-      this.players = {};
-
-      this.isGameOn = false;
-    }
-  }
-
-  _begin() {
-    const playersIterator = store.players.entries();
-
-    for (let el of playersIterator) {
-      const player = el[0];
-
-      if (!this.players[player]) {
-        this.players[player] = this.amount;
-      }
-    }
-
-    this._validate();
+  nextRound = () => {
+    this.round = roundLifeCycle();
   }
 }
 
